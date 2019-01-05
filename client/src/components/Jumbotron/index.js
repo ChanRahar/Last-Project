12be6aa0f27484bc86f2aefa74c9d1f80e5e9361
ChanR <chan.rahar@gmail.om>
2019-01-05 13:11:08 -0500

    worked on the index page, and use nav, jumbotron, and grid components

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>Play RPS Game Online</h1>
+      {/* <a target="_blank" rel="noopener noreferrer" href="https://developers.google.com/books/">
+        Search for and Save Books of Interest 
+      </a> */}
+    </div>
+  );
+}
+
+export default Jumbotron;
---
+++ client/src/pages/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// import Nav from "../components/Nav";
+import Jumbotron from "../components/Jumbotron";
+// import { Container, Row, Col } from "../components/Grid";
+
+class Main extends Component {
+
+    render() {
+        return(
+        <div>
+            <br></br>
+            <Jumbotron />
+        </div>
+        )
+    }
+}
+
+export default Main;
