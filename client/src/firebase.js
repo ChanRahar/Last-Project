5f8198b9fdf57c662180e7f991b58fdc59692f01
ChanR <chan.rahar@gmail.om>
2019-01-27 17:05:00 -0500

    heroku

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/database';
 // import firebaseKey from "./apiKey"
 
 const config = {
-    apiKey: process.env.firebasekey || firebaseKey.apiKey,
-    authDomain: process.env.firebaseauthDomain|| firebaseKey.authDomain,
-    databaseURL: process.env.firebasedatabaseURL || firebaseKey.databaseURL,
-    projectId: process.env.firebaseprojectId || firebaseKey.projectId,
-    storageBucket: process.env.firebasestorageBucket || firebaseKey.storageBucket,
-    messagingSenderId: process.env.firebasemessagingSenderId || firebaseKey.messagingSenderId
+    apiKey: process.env.firebasekey,
+    authDomain: process.env.firebaseauthDomain, 
+    databaseURL: process.env.firebasedatabaseURL,  
+    projectId: process.env.firebaseprojectId,  
+    storageBucket: process.env.firebasestorageBucket,  
+    messagingSenderId: process.env.firebasemessagingSenderId  
 };
 
 firebase.initializeApp(config);
