ee59c4ca8ca59c53de13ea52055dccb2279d4c55
ChanRahar <41398292+ChanRahar@users.noreply.github.com>
2019-02-17 14:32:28 -0500

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
-// import firebaseKey from "./apiKey"
+import firebaseKey from "./apiKey"
 
 const config = {
-    apiKey: process.env.firebasekey,
-    authDomain: process.env.firebaseauthDomain, 
+    apiKey: process.env.firebasekey || firebaseKey.apiKey,
+    authDomain: process.env.firebaseauthDomain || firebaseKey.authDomain, 
     databaseURL: "https://game-project-6e252.firebaseio.com",  
-    projectId: process.env.firebaseprojectId,  
-    storageBucket: process.env.firebasestorageBucket,  
-    messagingSenderId: process.env.firebasemessagingSenderId  
+    projectId: process.env.firebaseprojectId || firebaseKey.projectId,  
+    storageBucket: process.env.firebasestorageBucket || firebaseKey.storageBucket,  
+    messagingSenderId: process.env.firebasemessagingSenderId || firebaseKey.messagingSenderId  
 };
 
 firebase.initializeApp(config);
